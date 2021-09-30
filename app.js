40320e222ac3c9c211b9e0d893d2f2631c187adf
Darshil Dixit <this.is.darshil@gmail.com>
2021-09-30 14:04:13 +0530

    feat

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var dateOfBirth = document.querySelector("#date-of-birth");
 var luckyNumber = document.querySelector("#lucky-number");
 var checkButton = document.querySelector("#check-button");
+var outputMsg = document.querySelector("#output-msg");
 
 
 
@@ -9,9 +10,26 @@ checkButton.addEventListener("click", clickHandler);
 function clickHandler(){
     var dob = dateOfBirth.value;
     var sum = calculateSum(dob);
-}
+    if(dob && luckyNumber.value){
+        compareValues(sum, luckyNumber.value);
+    } else{
+        outputMsg.innerText = "Please enter both the fields"
+    }
+};
 
-function calculateSum (dob){
-    dob = dob.replaceAll("-", "");
-    
-}
+function calculateSum(dob){
+    dob =  dob.replaceAll("-", "");
+     let sum = 0;
+     for(var i=0; i<dob.length; i++){
+         sum = sum + Number(dob.charAt(i))
+     };
+    return sum;
+};
+
+function compareValues(sum, luckyNumber){
+    if(sum % luckyNumber === 0){
+      outputMsg.innerText = "Your birthday is lucky! 😀"
+    } else{
+        outputMsg.innerText = "Not that lucky 🙁"
+    }
+};
